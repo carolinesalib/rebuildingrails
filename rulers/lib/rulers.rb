1fdc4e4c2160b7034d663441419dda54d23a072b
Caroline <carolinesalibc@gmail.com>
2021-11-16 08:47:26 -0700

    Chapter 2 - Your First Controller

--- rulers/lib/rulers.rb
+++ rulers/lib/rulers.rb
@@ -1,12 +1,31 @@
 # frozen_string_literal: true
 require "rulers/array"
 require_relative "rulers/version"
+require "rulers/routing"
 
 module Rulers
   class Application
     def call(env)
-      `echo debug > debug.txt`;
-      [200, {"Content-Type" => "text/html"}, ["Hello from Ruby on Rulers!"]]
+      if env["PATH_INFO"] == "/favicon.ico"
+        return [404, {"Content-Type" => "text/html"}, []]
+      end
+
+      klass, act = get_controller_and_action(env)
+      p "####### #{klass.ancestors}"
+
+      controller = klass.new(env)
+      text = controller.send(act)
+      [200, {"Content-Type" => "text/html"}, [text]]
+    end
+  end
+
+  class Controller
+    def initialize(env)
+      @env = env
+    end
+
+    def env
+      @env
     end
   end
 end
