c0cad3d0508051b14c403e1006c00c060094e6d5
Caroline <carolinesalibc@gmail.com>
2021-11-17 08:11:55 -0700

    Chapter 2 - Exercise 2

--- rulers/lib/rulers.rb
+++ rulers/lib/rulers.rb
@@ -11,10 +11,16 @@ module Rulers
       end
 
       klass, act = get_controller_and_action(env)
-      p "####### #{klass.ancestors}"
-
       controller = klass.new(env)
-      text = controller.send(act)
+
+      begin
+        text = controller.send(act)
+      rescue
+        # Without this error interception we would see rack's default fancy error page
+        # with stack trace - Only on development mode though
+        return [500, {"Content-Type" => "text/html"}, ["Oh crap!!"]]
+      end
+
       [200, {"Content-Type" => "text/html"}, [text]]
     end
   end
