97d0845302dff8e642d18062920226092716bbef
Caroline <carolinesalibc@gmail.com>
2021-11-26 07:29:30 -0700

    Chapter 3 - Exercise 1

--- rulers/lib/rulers/dependencies.rb
+++ rulers/lib/rulers/dependencies.rb
@@ -1,6 +1,13 @@
 class Object
   def self.const_missing(c)
+    return nil if @calling_const_missing
+
+    # TODO: need to rewrite this in a diff way but I have no clue yet
+    @calling_const_missing = true
     require Rulers.to_underscore(c.to_s)
-    Object.const_get(c)
+    klass = Object.const_get(c)
+    @calling_const_missing = false
+
+    klass
   end
 end
