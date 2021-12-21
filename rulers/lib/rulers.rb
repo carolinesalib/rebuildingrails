5e3ac997da77193c9f00c785b55b0d5463149279
Caroline Salib <carolinesalibc@gmail.com>
2021-12-21 01:34:37 +0000

    Chapter 6 - Responses with Rack

--- rulers/lib/rulers.rb
+++ rulers/lib/rulers.rb
@@ -19,16 +19,14 @@ module Rulers
 
       klass, act = get_controller_and_action(env)
       controller = klass.new(env)
+      text = controller.send(act)
+      r = controller.get_response
 
-      begin
-        text = controller.send(act)
-      rescue
-        # Without this error interception we would see rack's default fancy error page
-        # with stack trace - Only on development mode though
-        # return [500, {"Content-Type" => "text/html"}, ["Oh crap!!"]]
+      if r
+        [r.status, r.headers, [r.body].flatten]
+      else
+        [200, {'Content-Type' => 'text/html'}, [text]]
       end
-
-      [200, {"Content-Type" => "text/html"}, [text]]
     end
   end
 end
